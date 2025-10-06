ca5ab63d07c19737a48f6a6a3d5109373f092c83
Ashlok Chaudhary <chaudharyashlok@gmail.com>
2025-10-06 14:27:35 +0530

    feat: add custom logging middleware

---
+++ internal/middlewares/logging.go
@@ -0,0 +1,17 @@
+package middlewares
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func Logger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		defer func() {
+			log.Printf("%s %s %s", r.Method, r.URL.Path, time.Since(start))
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
